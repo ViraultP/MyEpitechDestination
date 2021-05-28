4a1f73a94265c616a3cbacd3cd13af7878165337
Sarahamar-epitech <sarah.amar@epitech.eu>
2021-05-28 23:13:44 +0200

    Page Contact v2

--- Frontend/myepitechdestination-front/src/components/Contact.vue
+++ Frontend/myepitechdestination-front/src/components/Contact.vue
@@ -1,39 +1,65 @@
 <template>
-    <div class="container">
-        <div class="row">
-
-            <div class="col-md">
-                <h1>CONTACTEZ - NOUS</h1>
-                <p class="text">
-                    Contacter <a href="#">Epitech Toulouse </a> pour un problème sur le Site Web.
-                </p>
-                <p class="text">
-                    Si vous rechercher à obtenir plus de renseignements sur votre année à l’étranger, veuillez-vous rapprocher vers l’université partenaire de votre choix <a href="#">ici</a>.
-                </p>
-                <div class="row">
-                    <div class="col-sm">
-                        <h4>Adresse</h4>
-                        <p>40 boulevard de la marquette</p>
-                        <p>Toulouse (31 000)</p>
-                    </div>
-                    <div class="col-sm">
-                        <h4>E-Mail</h4>
-                        <p>toulouse@epitech.eu</p>
-                    </div>
+    <div>
+        <div class="header">
+            <!-- <img src="../assets/contact_plane.jpg" class="plane" alt="contact"> -->
+            <div class="row">
+                <div class="col-md-title">
+                    <h1>Contactez - nous</h1>
                 </div>
-                <div class="row">
-                    <div class="col-sm">
-                        <h4>Téléphone</h4>
-                        <p>01 44 08 00 15</p>
-                    </div>
+            </div>
+            <div class="row">
+                <div id="info" class="col-sm">
+                    <h4>Adresse</h4>
+                    <p>40 boulevard de la marquette</p>
+                    <p>Toulouse (31 000)</p>
+                </div>
+                <div id="info" class="col-sm">
+                   <h4>E-Mail</h4>
+                    <p>toulouse@epitech.eu</p>
+                </div>
+                <div id="info" class="col-sm">
+                   <h4>Téléphone</h4>
+                    <p>01 44 08 00 15</p>
                 </div>
             </div>
 
-            <div class="col-md">
-                <img src="../assets/phone.png" alt="Phone">
+        </div>
+        <div class="container">
+            <div id="contact_dev" class="row">
+                <div class="col-md-4">
+                    <img src="../assets/contact2.png" class="logo" alt="contact">
+                </div>
+                <div id="contact_dev_title" class="col-md-8" >
+                   <h1>Contactez les développeurs</h1>
+                </div>
+            </div>
+            
+            
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md" id="back">
+                    <h1>Developpeurs Back-End</h1>
+                    <h4>Pauline Virault</h4>
+                    <p>pauline.virault@epitech.eu</p>
+                    <h4>Vincent Crosnier</h4>
+                    <p>vincent.crosnier@epitech.eu</p>
+                    <h4>Jérôme Araspin</h4>
+                    <p>jerome.araspin@epitech.eu</p>
+                </div>
+                <div class="col-md" id="front">
+                    <h1>Developpeurs Front-End</h1>
+                    <h4>Andrea Rodriguez</h4>
+                    <p>andrea.rodriguez@epitech.eu</p>
+                    <h4>Sarah Amar</h4>
+                    <p>sarah.amar@epitech.eu</p>
+                </div>
             </div>
         </div>
+
     </div>
+                       
 </template>
 
 <script>
@@ -43,51 +69,83 @@ export default {
 </script>
 
 <style scoped>
-
-body{
-    background-color: red;
+.header{
+        background-image : url('../assets/contact_plane.jpg');
+        background-size: cover;;
+        background-repeat: repeat-x;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width : 100%;
+        height : auto;
+        margin-top : 0px;
+        margin-bottom : 0px;
+        padding : 0px;
+        z-index: -1;
 }
-.container{
-    margin-top: 20px;
-    background-color: #F2F2F2;
-    border-radius: 6px;
-}
 
-img{
+.plane{
     width: 100%;
     height: auto;
-    filter: drop-shadow(0px 20px 4px #7c7b7b);
-   
 }
 
-h1{
-    margin-top: 50px;
-    font-family: 'Bebas Neue', cursive;
-    font-size: 2.9em;
-    display: block;
+.col-md-title{
+     font-family: 'Bebas Neue', cursive;
     text-align: center;
-    color: #278867;
+    margin: 150px 0 0 0;
 }
 
-h4{
+.col-md-title h1{
+    font-size: 4.5em
+}
+
+#info{
+    margin: 300px 0 20px 0;
+    text-align: center;
+}
+
+#info p{
+    font-size: 1.3em;
+}
+/* ********************************* */
+#contact_dev h1{
+     font-family: 'Bebas Neue', cursive;
+     font-size: 4.2em;
+     padding-top: 50px;
+}
+
+/* ********************************* */
+
+#back{
+    padding-top: 50px;
+}
+
+#back h1{
     font-family: 'Bebas Neue', cursive;
+     font-size: 3.2em;
+     text-align: center;
+     color: #278867;
 }
 
-p{
-    font-weight: lighter;
-    text-align: justify;
+#back h4, p{
+    text-align: center;
 }
 
-.text{
-    padding-left: 10px;
-}
-.text a{
-    text-decoration: none;
-    color: #278867;
-    font-weight: bold;
+
+#front{
+    padding-top :50px;
 }
 
-.col-sm{
-    margin: 30px 0;
+#front h1{
+    font-family: 'Bebas Neue', cursive;
+     font-size: 3.2em;
+     text-align: center;
+     color: #278867;
 }
+
+#front h4, p{
+    text-align: center;
+}
+
+
 </style>
