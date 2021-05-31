0183d24ca3152f8fbd391958c5fea8ad0fa032c6
Pauline <pauline.virault@epitech.eu>
2021-05-31 15:01:40 +0200

    Edit front branch andrea

---
+++ Frontend/myepitechdestination-front/src/router.js
@@ -0,0 +1,78 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
+import Contact from "./components/Contact.vue";
+import Destination from "./components/Destination.vue";
+import NavTest from "./components/NavTest.vue";
+
+const Profile = () => import("./components/Profile.vue")
+const BoardAdmin = () => import("./components/BoardAdmin.vue")
+const BoardUser = () => import("./components/BoardUser.vue")
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  
+  {
+    path: "/home",
+    component: Home,
+  },
+  
+  {
+    path: "/navtest",
+    component: NavTest,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    component: Register,
+  },
+  {
+    path: "/contact",
+    component: Contact,
+  },
+  {
+    path: "/destination",
+    component: Destination,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: BoardAdmin,
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: BoardUser,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/home'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+  
+    if (authRequired && !loggedIn) {
+      next('/login');
+    } else {
+      next();
+    }
+  });
+
+export default router;
