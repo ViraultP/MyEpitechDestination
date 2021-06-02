5fe46a4f827907323c6587a0591695a58ca78f18
Andrea Rodriguez <andrea.rodriguez@epitech.eu>
2021-06-02 10:09:31 +0200

    ModificationsAndrea

---
+++ Frontend/Frontend/myepitechdestination-front/src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <p>hello from test</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+}
+// import $ from "jquery";
+
+
+
+
+</script>
+
+<style scoped>
+
+
+</style>
